32e97170bf5049c470284913e8f10f61eb79e0db
Rishabh <geetikamittal78@gmail.com>
2024-05-28 13:09:47 +0400

    added article link in awards

--- GGD Website/src/pages/awards/Awards.jsx
+++ GGD Website/src/pages/awards/Awards.jsx
@@ -9,7 +9,7 @@ export default function Awards() {
             <Award img="6" title='Recognized as "Green Climate Ambassador" for Enviroserve UAE'/>
             <Award img="5" title="Certificate of Recognition for World Environment Day - Collected and Recycled 1,007 Kgs of E-waste"/>
             <Award img="3" title="Dubai International Academy, Emirates Hills - End Of Year Recognition Award for Sustainability"/>
-            <Award img="2" title="Coverage in The National News, on how a sustainable lifestyle can help grow your wealth"/>
+            <Award img="2" title={<a href="https://www.thenationalnews.com/weekend/2022/02/11/how-a-sustainable-lifestyle-can-help-grow-your-wealth/">Coverage in The National News, on how a sustainable lifestyle can help grow your wealth</a>}/>
             <Award img="4" title='Recognized as a "ZeLoop Ambassador" on International Youth Day' />
             <Award img="1" title="Recognized for valued participation and collecting 41+ kgs of plastic"/>
         </div>
